52fdb41f20e131df1f7044ccbd295649a10f9fc8
Christian Meichsner <christian.meichsner@live.com>
2014-04-01 16:29:48 +0200

    Create make-fms.bat

---
+++ make-fms.bat
@@ -0,0 +1,35 @@
+@ECHO OFF
+set target=fsm
+set target-temp=fsm-temp
+echo Lambdaroyal FSM Installer - this script downloads and builds the software into %target%. Using 
+
+if exist %target% (
+	echo Removing directory %target%
+	rmdir /S /Q %target%
+)
+if exist %target-temp% (
+	echo Removing directory %target-temp%
+	rmdir /S /Q %target-temp%
+)
+echo Creating directory %target%
+echo Creating directory %target-temp%
+
+
+echo Downloading the sources
+git clone https://github.com/lambdaroyal/fms %target-temp%
+cd %target-temp%
+echo Building fsm
+lein jar
+lein uberjar
+
+cp samples ../%target%
+cp target *standalone.jar ../%target%
+
+echo Removing directory %target%
+rmdir /S /Q %target-temp%
+
+echo Find the software in %target%
+echo You may start the software using java -jar wus-0.1-SNAPSHOT-standalone.jar --path ../samples
+pause
+
+
